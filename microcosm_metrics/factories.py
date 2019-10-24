e8363fa7d0d4eaa3d9d184904ee92567ad290d25
KensoDev <avi@kensodev.com>
2019-10-24 15:27:38 -0700

    Fixing missing comma in metrics name

--- microcosm_metrics/factories.py
+++ microcosm_metrics/factories.py
@@ -30,7 +30,7 @@ def configure_metrics(graph, configuration="metrics"):
         host=config.host,
         port=config.port,
         constant_tags=[
-            f"service:{metric_service_name}"
+            f"service:{metric_service_name}",
             # An empty string is *not* a valid value: tags cannot end with a colon.
             # An environment variable can be set to an empty string. We force empty strings
             # into "undefined".
